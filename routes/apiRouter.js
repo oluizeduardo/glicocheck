252817586e0d0801a8f48084ab5b3d013a4b6693
Luiz Eduardo da Costa <luiz_costa@hst.br>
2022-07-20 21:30:17 -0300

    Add select users by id.

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -16,4 +16,18 @@ apiRouter.get('/users', function (req, res) {
     knex.select('*').from('users').then(users => res.json(users))
 })
 
+apiRouter.get('/users/:id', function (req, res) {
+    let id = Number.parseInt(req.params.id);
+    knex.select('name', 'email', 'roles')
+        .from('users')
+        .where('id',id)
+        .then(users => {
+            if(users.length){
+                res.json(users);
+            }else{
+                res.status(404).json({message: `User with id ${id} was not found.`})
+            }
+        });
+})
+
 module.exports = apiRouter
