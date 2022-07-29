094baa22785ad17a0297ed8b00c10eb87611f1fb
Luiz Eduardo da Costa <luiz_costa@hst.br>
2022-07-28 23:55:38 -0300

    Added list of common messages.

---
+++ messages.js
@@ -0,0 +1,11 @@
+class Messages{
+    static NOTHING_FOUND = "Nothing found";
+    static TOKEN_REQUIRED = 'Access token required';
+    static TOKEN_EXPIRED = "Token expired";
+    static REFUSED_ACCESS = "Refused access";
+    static ROLE_ADMIN_REQUIRED = 'Role ADMIN is required';
+    static REGISTER_DELETED = 'The register has been deleted';
+    static ERROR_DELETE_USER = 'Error trying to delete user.';
+}
+
+module.exports = Messages;
