2802ded1184d03648d2695b95e9415712ad6bf44
Luiz Eduardo da Costa <luiz_costa@hst.br>
2022-07-29 10:32:09 -0300

    Updated text messages source showed in responses.

--- messages.js
+++ messages.js
@@ -6,6 +6,8 @@ class Messages{
     static ROLE_ADMIN_REQUIRED = 'Role ADMIN is required';
     static REGISTER_DELETED = 'The register has been deleted';
     static ERROR_DELETE_USER = 'Error trying to delete user.';
+    static ERROR_UPDATE_GLUCOSE = 'Error trying to update glucose reading.';
+    static ERROR_DELETE_GLUCOSE = 'Error trying to delete glucose reading.';
 }
 
 module.exports = Messages;
--- routes/glucoseRouter.js
+++ routes/glucoseRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const glucoseRouter = express.Router();
+const Messages = require('../messages');
 
 // CONFIG TO CONNECT WITH THE DATABASE.
 const knex = require ('knex') ({
@@ -14,19 +15,11 @@ const knex = require ('knex') ({
 })
 
 
-const NOTHING_FOUND = "Nothing found";
-const TOKEN_REQUIRED = 'Access token required';
-const TOKEN_EXPIRED = "Token expired";
-const REFUSED_ACCESS = "Refused access";
-const ROLE_ADMIN_REQUIRED = 'Role ADMIN is required';
-const REGISTER_DELETED = 'The register has been deleted';
-
-
 // Authenticate password
 let checkToken = (req, res, next) => {
     let authToken = req.headers['authorization'];
     if (!authToken) {        
-        res.status(401).json({ message: TOKEN_REQUIRED});
+        res.status(401).json({ message: Messages.TOKEN_REQUIRED});
         return;
     }
     else {
@@ -37,10 +30,10 @@ let checkToken = (req, res, next) => {
     jwt.verify(req.token, process.env.SECRET_KEY, (err, decodeToken) => {
         if (err) {
             if(err.name === 'TokenExpiredError'){
-                res.status(401).json({ message: TOKEN_EXPIRED, expiredIn: err.expiredAt});
+                res.status(401).json({ message: Messages.TOKEN_EXPIRED, expiredIn: err.expiredAt});
                 return
             }
-            res.status(401).json({ message: REFUSED_ACCESS });
+            res.status(401).json({ message: Messages.REFUSED_ACCESS });
             return
 
         }
@@ -63,7 +56,7 @@ let isAdmin = (req, res, next) => {
                     return
                 }
                 else {
-                    res.status(403).json({ message: ROLE_ADMIN_REQUIRED })
+                    res.status(403).json({ message: Messages.ROLE_ADMIN_REQUIRED })
                     return
                 }
             }
@@ -89,7 +82,7 @@ glucoseRouter.get('/', checkToken, function (req, res) {
             if(glucoses.length){
                 res.status(200).json(glucoses);
             }else{
-                res.status(404).json({message: NOTHING_FOUND})
+                res.status(404).json({message: Messages.NOTHING_FOUND})
             }
         });
 })
@@ -111,7 +104,7 @@ glucoseRouter.get('/user/:user_id', checkToken, function (req, res) {
             if(glucoses.length){
                 res.status(200).json(glucoses);
             }else{
-                res.status(404).json({message: NOTHING_FOUND})
+                res.status(404).json({message: Messages.NOTHING_FOUND})
             }
         });
 })
@@ -132,7 +125,7 @@ glucoseRouter.get('/:id', checkToken, function (req, res) {
             if(glucoses.length){
                 res.status(200).json(glucoses);
             }else{
-                res.status(404).json({message: NOTHING_FOUND})
+                res.status(404).json({message: Messages.NOTHING_FOUND})
             }
         });
 })
@@ -153,7 +146,7 @@ glucoseRouter.get('/markermeal/:markermealid', checkToken, function (req, res) {
             if(glucoses.length){
                 res.status(200).json(glucoses);
             }else{
-                res.status(404).json({message: NOTHING_FOUND})
+                res.status(404).json({message: Messages.NOTHING_FOUND})
             }
         });
 })
@@ -199,11 +192,11 @@ glucoseRouter.put('/:id', express.json(), checkToken, isAdmin, function (req, re
                 let glucose = glucoses[0]
                 res.status(201).json({glucose})
             }else{
-                res.status(404).json({message: NOTHING_FOUND})
+                res.status(404).json({message: Messages.NOTHING_FOUND})
             }
         })
         .catch (err => res.status(500)
-        .json ({ message: `Error trying to update glucose reading. ERROR: ${err.message}`}))
+        .json ({ message: Messages.ERROR_UPDATE_GLUCOSE + ` ERROR: ${err.message}`}))
 })
 
 
@@ -215,11 +208,11 @@ glucoseRouter.delete('/:id', checkToken, isAdmin, function (req, res) {
         knex('glucose')
           .where('id', id)
           .del()
-          .then(res.status(200).json({message: REGISTER_DELETED }))
-          .catch (err => res.status(500).json ({ message: `Error trying to delete glucose reading. ERROR: ${err.message}`}))
+          .then(res.status(200).json({message: Messages.REGISTER_DELETED }))
+          .catch (err => res.status(500).json ({ message: Messages.ERROR_DELETE_GLUCOSE + ` ERROR: ${err.message}`}))
     } else {
         res.status(404).json({
-            message: NOTHING_FOUND
+            message: Messages.NOTHING_FOUND
         })
     }        
 })
