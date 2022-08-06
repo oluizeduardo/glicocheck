62ba7e9050229328d5ebb5572901b0eff83c4ca8
Luiz Eduardo da Costa <luiz_costa@hst.br>
2022-08-06 19:20:02 -0300

    Updated chart with hypo and hyper values.

--- public/includes/js/dashboard.js
+++ public/includes/js/dashboard.js
@@ -8,7 +8,7 @@
   // Graphs
   const ctx = document.getElementById('myChart')
   // eslint-disable-next-line no-unused-vars
-  const myChart = new Chart(ctx, {
+  /*const myChart = new Chart(ctx, {
     type: 'line',
     data: {
       labels: [
@@ -49,5 +49,44 @@
         display: false
       }
     }
-  })
+  })*/
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['glycemia1', 'glycemia2', 'glycemia3', 'glycemia4', 'glycemia5', 'glycemia6', 'glycemia7'],
+        datasets: [
+        {
+          label: 'Hyperglycemia',
+          data: [160, 160,160,160, 160, 160, 160],
+          borderColor: [              
+              'rgba(5, 172, 228, 1)'
+          ],
+          borderWidth: 2
+        },
+        {
+          label: 'My glycemia',
+          data: [63, 90, 138, 187, 140, 100, 103],
+          borderColor: [
+            'rgba(0, 0, 0, 2)'
+          ],
+          borderWidth: 2
+        },        
+        {
+          label: 'Hypoglycemia',
+          data: [70, 70,70,70, 70, 70, 70],
+          borderColor: [
+            'rgba(255, 99, 132, 1)'
+          ],
+          borderWidth: 2
+      }
+      ]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+})
 })()
