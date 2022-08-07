f71a46ca1be94532ebced33826edcdefeab47148
Luiz Eduardo da Costa <luiz_costa@hst.br>
2022-08-07 20:31:50 -0300

    Sidemenu dashboard CSS.

--- public/includes/js/dashboard.js
+++ public/includes/js/dashboard.js
@@ -1,55 +1,8 @@
-/* globals Chart:false, feather:false */
+const ctx = document.getElementById('myChart');
 
-(() => {
-  'use strict'
-
-  feather.replace({ 'aria-hidden': 'true' })
-
-  // Graphs
-  const ctx = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
-  /*const myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        '04-Aug-22, 07:22',
-        '04-Aug-22, 12:13',
-        '04-Aug-22, 14:13',
-        '04-Aug-22, 17:05',
-        '04-Aug-22, 19:30',
-        '04-Aug-22, 21:30',
-        '04-Aug-22, 23:57'
-      ],
-      datasets: [{
-        data: [
-          98,
-          87,
-          154,
-          116,
-          94,
-          138,
-          99
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
-    }
-  })*/
+function loadChart(){
+  //myLineChart.update();
+  //myLineChart.reset();
   const myChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -84,9 +37,14 @@
     options: {
         scales: {
             y: {
-                beginAtZero: true
+                min: 40,
+                ticks: {
+                  stepSize: 20
+                }
             }
         }
     }
-})
-})()
+  });
+}
+
+loadChart();
