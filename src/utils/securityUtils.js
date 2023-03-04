e659e6f053751987c2e9fdf06aba0d038aa6b7a2
Luiz Eduardo da Costa <luiz_costa@hst.br>
2023-03-04 00:41:58 -0300

    Added securityUtils

---
+++ src/utils/securityUtils.js
@@ -0,0 +1,60 @@
+const Messages = require('../utils/messages');
+const jwt = require('jsonwebtoken');
+const database = require('../db/dbconfig.js');
+
+class SecurityUtils {
+
+    static checkToken = (req, res, next) => {
+        let authToken = req.headers['authorization'];
+        if (!authToken) {        
+            res.status(401).json({ message: Messages.TOKEN_REQUIRED});
+            return;
+        }
+        else {
+            let token = authToken.split(' ')[1];
+            req.token = token;
+        }
+    
+        jwt.verify(req.token, process.env.SECRET_KEY, (err, decodeToken) => {
+            if (err) {
+                if(err.name === 'TokenExpiredError'){
+                    res.status(401).json({ message: Messages.TOKEN_EXPIRED, expiredIn: err.expiredAt});
+                    return
+                }
+                res.status(401).json({ message: Messages.REFUSED_ACCESS });
+                return
+    
+            }
+            req.userId = decodeToken.id
+            next()
+        })
+    };
+    
+    // CHECK USER ROLE.
+    static isAdmin = (req, res, next) => {
+        database
+            .select ('*').from ('users').where({ id: req.userId })
+            .then ((users) => {
+                if (users.length) {
+                    let user = users[0]
+                    let role = user.role_id;
+                    
+                    if (role === 1) {
+                        next()
+                        return
+                    }
+                    else {
+                        res.status(403).json({ message: Messages.ROLE_ADMIN_REQUIRED })
+                        return
+                    }
+                }
+            })
+            .catch (err => {
+                res.status(500).json({ 
+                  message: Messages.ERROR_CHECKING_USER_ROLE,
+                  error: err.message })
+            })
+    };
+}
+
+module.exports = SecurityUtils;
