0f320a961dfcc5c29d1e61d832457b8b6ebdd21a
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2023-03-04 19:10:24 -0300

    Added password verification during user's account delete.

--- src/public/includes/js/deleteAccount.js
+++ src/public/includes/js/deleteAccount.js
@@ -1,21 +1,24 @@
 let btnConfirmDeleteAccount = document.getElementById("btnConfirmDeleteAccount");
 let field_delete_account_Password = document.getElementById("field_delete_account_Password");
+let isCorrectUserPassword = false;
 
 btnConfirmDeleteAccount.addEventListener('click', () => {
-    // In the future check user's password before deleting the account.
-    if(field_delete_account_Password.value){
-        const xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = () => 
-        {
-            if (xmlhttp.readyState == XMLHTTPREQUEST_STATUS_DONE && xmlhttp.status == OK) 
+
+    checkUserPassword((isCorrectPassword) => {
+        if(isCorrectPassword){
+            const xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = () => 
             {
-                deleteUserAccount();
-            }
-        };
-        sendRequestToDeleteGlucoseReadings(xmlhttp);
-    }else{
-        swal("Password needed", 'Please, type the correct password to delete your account.', "warning");
-    }
+                if (xmlhttp.readyState == XMLHTTPREQUEST_STATUS_DONE && xmlhttp.status == OK) 
+                {
+                    deleteUserAccount();
+                }
+            };
+            sendRequestToDeleteGlucoseReadings(xmlhttp);
+        }else{
+            swal("Password needed", 'Please, type the correct password to delete your account.', "warning");
+        }
+    });
 });
 
 function sendRequestToDeleteGlucoseReadings(xmlhttp){
@@ -56,4 +59,22 @@ function sendRequestToDeleteUserAccount(xmlhttp){
     xmlhttp.setRequestHeader('Authorization', 'Bearer '+token);
     xmlhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     xmlhttp.send();
+}
+
+function checkUserPassword(onLoad){
+    const userId = getUserId();
+    const password = field_delete_account_Password.value;
+
+    let json = JSON.stringify({
+        "userId": userId,
+        "password": password
+      });
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/api/security/password/validation");
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.send(json);
+    xhr.onload = () => {
+        onLoad(JSON.parse(xhr.response).result);
+    }
 }
