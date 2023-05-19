f3611cc62107a8c8fc6b18798c12bde0d5c1c8e5
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2023-05-18 23:16:52 -0300

    Configuring Cypress to run the integration tests.

---
+++ cypress/e2e/integration/systemHealthCheckController.cy.js
@@ -0,0 +1,12 @@
+describe('System health check test', () => {
+  it('should return HTTP 200 and the message \"Pong\" informing that the system is running.', () => {
+    cy.request({
+      method: 'GET',
+      url: 'http://localhost:4500/api/ping',
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('message');
+      expect(response.body.message).to.be.equal('Pong');
+    });
+  });
+});
