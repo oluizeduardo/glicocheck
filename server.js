4de1711901ce09379e9e67f307160be5eb86e9af
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2024-10-09 20:51:08 -0300

    Added Cache-Control header

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ if (environment === 'dev') {
   });
 }
 
+// Prevents the browser from storing pages in the cache.
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store');
+  next();
+});
+
 app.use(express.json({limit: '2mb'}));
 app.use(express.urlencoded({extended: true}));
 app.use('/', express.static(path.join(__dirname, '/src/public')));
