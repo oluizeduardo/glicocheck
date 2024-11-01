3d47b90218d6d4cd4acd05b468a44f1718ae49d3
Luiz Eduardo da Costa <costaeduardoluiz@gmail.com>
2024-11-01 15:24:15 -0300

    When 404, changed the return page to home.html

--- server.js
+++ server.js
@@ -20,15 +20,15 @@ if (environment === 'dev') {
   });
 }
 
+app.use(express.json({limit: '2mb'}));
+app.use(express.urlencoded({extended: true}));
+app.use('/', express.static(path.join(__dirname, '/src/public')));
+
 // Prevents the browser from storing pages in the cache.
 app.use((req, res, next) => {
   res.set('Cache-Control', 'no-store');
   next();
 });
-
-app.use(express.json({limit: '2mb'}));
-app.use(express.urlencoded({extended: true}));
-app.use('/', express.static(path.join(__dirname, '/src/public')));
 
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, '/src/public/404.html'));
